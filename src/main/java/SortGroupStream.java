302ac38d9191e60fcffe14ccc4c838d5bf57a8b6
Andrey <titanmet@mail.ru>
2019-02-08 17:30:10 +0300

    Update and rename new.java to SortGroupStream.java

--- src/main/java/SortGroupStream.java
+++ src/main/java/SortGroupStream.java
@@ -1,18 +1,17 @@
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.Comparator;
 
-class Main {
+class SortGroupStream {
 
-       public static void main(String[] args){
+    public static void main(String[] args){
 
         String str="сапог семга сарай зоология вольт занзибар арбуз болт бокс биржа аэропорт филин ворона бородач аэлита вал сэр скоро завтрак зов лев клоун";
 
         ArrayList<String> wordList = new ArrayList<>(Arrays.asList(str.split(" ")));
         List<String> letterList = new ArrayList<>();
-        for (String s: wordList){ 
-        letterList.add(s.substring(0,1));
+        for (String s: wordList){
+            letterList.add(s.substring(0,1));
         }
         Map<String, Long> letterMap = letterList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         letterList.removeIf(p -> letterMap.get(p).intValue() == 1);
@@ -25,31 +24,32 @@ class Main {
         }
         List<String> fs=new ArrayList<String>();
         for(String s: letterList)
-        fs.add(s);
+            fs.add(s);
         List<String> wd=new ArrayList<String>();
         for(String s: wordsList)
-        wd.add(s);
+            wd.add(s);
         List<String> firstsLetter = fs.stream().distinct().sorted().collect(Collectors.toList());
-        List<String> words = wd.stream().sorted((o1,o2)->o2.compareTo(o1)).distinct().collect(Collectors.toList());
-        
+        List<String> words = wd.stream().sorted((o1,o2)->o2.length()-(o1.length())).distinct().collect(Collectors.toList());
         StringBuilder result=new StringBuilder();
         StringBuilder key=new StringBuilder();
-
         Map<StringBuilder,StringBuilder> group=new HashMap<>();
         System.out.print("[ ");
         for (int i = 0; i < firstsLetter.size(); i++) {
             for (int j = 0; j < words.size(); j++) {
                 if ((firstsLetter.get(i).equals(words.get(j).substring(0, 1))))
-                        result.append(words.get(j) + ",");
+                    result.append(words.get(j) + ",");
             }
             result.deleteCharAt(result.length()-1);
             key.append(firstsLetter.get(i)+"=");
-            group.put(key,result);
-            for (Map.Entry<StringBuilder,StringBuilder> item: group.entrySet())
-                System.out.print(item.getKey()+"["+item.getValue()+"] ");
-            key.delete(0,result.length());
-            result.delete(0, result.length());
+            tes(result, key, group);
         }
         System.out.print("]");
+    }
+    static void tes(StringBuilder result, StringBuilder key, Map<StringBuilder, StringBuilder> group) {
+        group.put(key,result);
+        for (Map.Entry<StringBuilder,StringBuilder> item: group.entrySet())
+            System.out.print(item.getKey()+"["+item.getValue()+"] ");
+        key.delete(0,result.length());
+        result.delete(0, result.length());
     }
 }
